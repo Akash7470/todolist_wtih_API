71bcf0b7efd0f6a016e297dc813a7738caa0dd96
Chandanrajput082 <chandan.r@webkorps.com>
2023-03-18 01:41:19 +0530

    home file changes

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -18,7 +18,7 @@ function Data() {
   // For Deleting the data --
   const deleteTodos = async (id) => {
     const res = await axios.delete(`http://localhost:5000/todos/${id}`);
-    console.log(res + "Deleted...");
+    console.log(res + "Deleted......");
     getTodos();
   };
 
@@ -58,7 +58,7 @@ function Data() {
       {data?.map((data, index) => {
         return (
           <div className="d-inline-flex flex-wrap" key={index}>
-            <div className="card bg-info m-3" style={{ width: "18rem" }}>
+            <div className="card bg-info m-4" style={{ width: "18rem" }}>
               <div
                 id="circle"
                 style={{
@@ -66,7 +66,7 @@ function Data() {
                   height: "2rem",
                   borderRadius: "10%",
                   border: "1px solid #222",
-                  margin: "10px",
+                  margin: "12px",
                   textAlign: "center",
                 }}
               >
